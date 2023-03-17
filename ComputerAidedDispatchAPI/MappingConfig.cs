309c3d93e8601a5e38a27e255139d5c8e3c37d9d
William Applegate <w.applegate@ymail.com>
2023-03-16 22:49:05 -0400

    Mapping config changes: Removed reverse mapping where not applicable and checked for null

--- ComputerAidedDispatchAPI/MappingConfig.cs
+++ ComputerAidedDispatchAPI/MappingConfig.cs
@@ -12,26 +12,27 @@ namespace ComputerAidedDispatchAPI
         {
             CreateMap<ApplicationUser, UserDTO>().ReverseMap();
             
-            CreateMap<Unit, UnitReadDTO>().ReverseMap();
+            CreateMap<Unit, UnitReadDTO>();
 
             CreateMap<Unit, UnitDetailsReadDTO>()
                 .ForMember(dto => dto.Name, act => act.MapFrom(src => src.UserInfo.Name))
-                .ForMember(dto => dto.CallForService, cfs => cfs.MapFrom(src => new CallForServiceReadDTO()
-                {
-                    Id = src.CallForService.Id,
-                    CallType = src.CallForService.CallType,
-                    Address = src.CallForService.Address,
-                    Status = src.CallForService.Status,
-                    Description = src.CallForService.Description,
-                    DateTimeCreated = src.CallForService.DateTimeCreated,
-                    Units = src.CallForService.Units.Select(unit => new UnitReadDTO()
+                .ForMember(dto => dto.CallForService, cfs => cfs.MapFrom(src => src.CallForService == null ?
+                    null :
+                    new CallForServiceReadDTO()
                     {
-                        UnitNumber = unit.UnitNumber,
-                        CallNumber = unit.CallNumber,
-                        Status = unit.Status
-                    }).ToList()
-                }))
-                .ReverseMap();
+                        Id = src.CallForService.Id,
+                        CallType = src.CallForService.CallType,
+                        Address = src.CallForService.Address,
+                        Status = src.CallForService.Status,
+                        Description = src.CallForService.Description,
+                        DateTimeCreated = src.CallForService.DateTimeCreated,
+                        Units = src.CallForService.Units.Select(unit => new UnitReadDTO()
+                        {
+                            UnitNumber = unit.UnitNumber,
+                            CallNumber = unit.CallNumber,
+                            Status = unit.Status
+                        }).ToList()
+                }));
 
             CreateMap<CallForService, CallForServiceReadDTO>()
                 .ForMember(dto => dto.Units, act => act.MapFrom(src => src.Units.Select(unit => new UnitReadDTO()
